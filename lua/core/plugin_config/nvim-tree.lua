1a18c05cf022d5db44e90d65ea5a6040e005c584
abiralstha <abi.sthapit@gmail.com>
2023-04-14 08:27:34 +0545

    feat(nvim): nvim lua setup

---
+++ lua/core/plugin_config/nvim-tree.lua
@@ -0,0 +1,17 @@
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
+
+require("nvim-tree").setup({
+  sort_by = "case_sensitive",
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = false,
+  },
+})
+
+vim.keymap.set('n', '<c-n>', ':NvimTreeFindFileToggle<CR>')
